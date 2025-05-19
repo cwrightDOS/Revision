bef6f6d9b76cd306ea07acfd565c59b6a4d04233
cwrightDOS <christopher.wright@baysgarthschool.co.uk>
2025-05-19 15:19:35 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
